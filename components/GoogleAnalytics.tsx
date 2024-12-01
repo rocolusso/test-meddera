beaf1f7c9f38be76e7fe8eb60362e9a7ea214047
Rocolusso <prolight@prolights-MacBook-Pro.local>
2024-12-01 23:15:37 +0200

    update google analytics 4

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -5,39 +5,39 @@ const GoogleAnalytics = () => {
     const gtag = 'G-V98H1SG9KW'
     return (
         <>
-          {/*  <Script*/}
-          {/*      strategy='lazyOnload'*/}
-          {/*      // src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}*/}
-          {/*      src={`https://www.googletagmanager.com/gtag/js?id=${gtag}`}*/}
-          {/*  />*/}
+            <Script
+                strategy='lazyOnload'
+                // src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag}`}
+            />
 
-          {/*  <Script id='' strategy='lazyOnload'>*/}
-          {/*      {`*/}
-          {/*    window.dataLayer = window.dataLayer || [];*/}
-          {/*    function gtag(){dataLayer.push(arguments);}*/}
-          {/*    gtag('js', new Date());*/}
-          {/*    gtag('config', '${gtag}', {*/}
-          {/*    page_path: window.location.pathname,*/}
-          {/*    });*/}
-          {/*`}*/}
-          {/*  </Script>*/}
-
-
-
-
-            <Script  strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-V98H1SG9KW"></Script>
-            <Script  strategy="afterInteractive">
+            <Script id='' strategy='lazyOnload'>
                 {`
-                
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', 'G-V98H1SG9KW');
-                
-                
-                `}
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${gtag}', {
+              page_path: window.location.pathname,
+              });
+          `}
             </Script>
+
+
+
+
+            {/*<Script  strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-V98H1SG9KW"></Script>*/}
+            {/*<Script  strategy="afterInteractive">*/}
+            {/*    {`*/}
+            {/*    */}
+            {/*    window.dataLayer = window.dataLayer || [];*/}
+            {/*    function gtag(){dataLayer.push(arguments);}*/}
+            {/*    gtag('js', new Date());*/}
+
+            {/*    gtag('config', 'G-V98H1SG9KW');*/}
+            {/*    */}
+            {/*    */}
+            {/*    `}*/}
+            {/*</Script>*/}
         </>
     );
 };
