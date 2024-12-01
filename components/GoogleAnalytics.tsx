e8d76345202391905e96fe49264f4b3dbed824a6
Rocolusso <prolight@prolights-MacBook-Pro.local>
2024-12-01 23:06:01 +0200

    update google analytics 2

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -5,21 +5,38 @@ const GoogleAnalytics = () => {
     const gtag = 'G-V98H1SG9KW'
     return (
         <>
-            <Script
-                strategy='lazyOnload'
-                // src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
-                src={`https://www.googletagmanager.com/gtag/js?id=${gtag}`}
-            />
+          {/*  <Script*/}
+          {/*      strategy='lazyOnload'*/}
+          {/*      // src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}*/}
+          {/*      src={`https://www.googletagmanager.com/gtag/js?id=${gtag}`}*/}
+          {/*  />*/}
 
-            <Script id='' strategy='lazyOnload'>
+          {/*  <Script id='' strategy='lazyOnload'>*/}
+          {/*      {`*/}
+          {/*    window.dataLayer = window.dataLayer || [];*/}
+          {/*    function gtag(){dataLayer.push(arguments);}*/}
+          {/*    gtag('js', new Date());*/}
+          {/*    gtag('config', '${gtag}', {*/}
+          {/*    page_path: window.location.pathname,*/}
+          {/*    });*/}
+          {/*`}*/}
+          {/*  </Script>*/}
+
+
+
+
+            <Script async  strategy='lazyOnload' src="https://www.googletagmanager.com/gtag/js?id=G-V98H1SG9KW"></Script>
+            <Script  strategy='lazyOnload'>
                 {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${gtag}', {
-              page_path: window.location.pathname,
-              });
-          `}
+                
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-V98H1SG9KW');
+                
+                
+                `}
             </Script>
         </>
     );
