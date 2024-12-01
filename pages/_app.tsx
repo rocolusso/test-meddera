d03fbca99778ab1545d5b658c6e2204de380d5d5
Rocolusso <prolight@prolights-MacBook-Pro.local>
2024-12-01 23:17:10 +0200

    update google analytics 5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
@@ -9,6 +10,9 @@ export default function App({ Component, pageProps }: AppProps) {
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no" />
   </Head>
+
+  <GoogleAnalytics/>
+
   <Component {...pageProps} />
 
   </>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 export default function Document() {
   return (
     <Html lang="en">
         <Head/>
-        <GoogleAnalytics/>
         <body className="antialiased">
         <Main/>
         <NextScript />
