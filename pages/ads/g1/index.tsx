85243573991ccc9336c76e3dfd50d7185fcf0ef8
Rocolusso <prolight@prolights-MacBook-Pro.local>
2024-12-05 05:17:39 +0200

    updated contacts map iframe

--- pages/ads/g1/index.tsx
+++ pages/ads/g1/index.tsx
@@ -7,6 +7,9 @@ import { FaPhoneVolume } from "react-icons/fa6";
 import { FaArrowRight } from "react-icons/fa";
 import imgAddress from "@/public/assets/img/img_contacts.jpg";
 
+import cert from './../../../public/assets/img/cert/background.svg'
+
+
 const GoogleWhitePage = () => {
     const date = new Date();
     const currentYear = date.getFullYear()
@@ -188,24 +191,37 @@ const GoogleWhitePage = () => {
                         <div className="mapouter">
                             <div className="gmap_canvas">
                                 <iframe frameBorder="0" scrolling="no"
+                                        title={'contact map'}
                                         className={'max-w-[100%] gmap_iframe'}
                                         width="2000"
                                         height="450"
-                                        src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=Дерматолог Бельцы Екатерина Пынтя Dermatolog Balti Meddera Ecaterina Pintea Magnific Nord Incomed&amp;t=&amp;z=18&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+                                        src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=B%C4%83l%C8%9Bi%20MD,%20Strada%20%C8%98tefan%20cel%20Mare%2013,%20MD-3100,%20%D0%9C%D0%BE%D0%BB%D0%B4%D0%BE%D0%B2%D0%B0+(%D0%94%D0%B5%D1%80%D0%BC%D0%B0%D1%82%D0%BE%D0%BB%D0%BE%D0%B3%20%D0%91%D0%B5%D0%BB%D1%8C%D1%86%D1%8B%20%7C%20Meddera%20%7C%20%D0%95%D0%BA%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%BD%D0%B0%20%D0%9F%D1%8B%D0%BD%D1%82%D1%8F%20%7C%20Dermatolog%20Balti%20%7C%20Ecaterina%20Pinteaness%20Name)&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
                             </div>
                         </div>
                     </div>
+
+
+
+                    {/*<div className={'voucher bg-white flex justify-center'}>*/}
+                    {/*    <div className={'relative'}>*/}
+                    {/*        <div className={'cert-number absolute top-5 left-6 sm:top-11  sm:left-16 text-white '}>*/}
+                    {/*            <p className={'text-[10px] sm:text-[1.8rem]'}>67868</p>*/}
+                    {/*        </div>*/}
+                    {/*        <Image src={cert} alt={'certificate'} width={1000} height={1000}/>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+
 
                     <div className={'footer_bottom bg-black'}>
                         <div
                             className={'container mx-auto  text-center flex flex-col max-w-[400px]  p-5  text-white'}>
 
-                                <p>{`© Meddera | Ecaterina Pintea | 2018 - ${currentYear}`}</p>
+                            <p>{`© Meddera | Ecaterina Pintea | 2018 - ${currentYear}`}</p>
 
 
                             <div className={' my-10 '}>
 
-                                    <p>{`Website by Alex`}</p>
+                                <p>{`Website by Alex`}</p>
 
                             </div>
                         </div>
