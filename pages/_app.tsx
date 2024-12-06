bafc9406a6e3932625a1bdc4f4c2c02ad819b720
Rocolusso <prolight@prolights-MacBook-Pro.local>
2024-12-06 23:34:26 +0200

    removed test recaptcha

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,27 +11,6 @@ export default function App({ Component, pageProps }: AppProps) {
 <>
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no"/>
-    <Script
-        strategy={'afterInteractive'}
-        src="https://www.google.com/recaptcha/enterprise.js?render=6LdOX5QqAAAAALaHFmLKGfZQLPI6RpuX1bU10r9b">
-
-    </Script>
-
-    <Script>
-      {`
-      
-       function onClick(e) {
-    e.preventDefault();
-    grecaptcha.enterprise.ready(async () => {
-      const token = await grecaptcha.enterprise.execute('6LdOX5QqAAAAALaHFmLKGfZQLPI6RpuX1bU10r9b', {action: 'LOGIN'});
-    });
-  }
-      
-      
-      `}
-
-    </Script>
-
 
 
   </Head>
