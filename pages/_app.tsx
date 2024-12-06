7899e7eee6e5705f82ff0a74c9090bf3b6c8f910
Rocolusso <prolight@prolights-MacBook-Pro.local>
2024-12-06 23:32:02 +0200

    added for test recaptcha

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,36 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 import GoogleTagManager from "@/components/GoogleTagManager";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
 
 <>
   <Head>
-    <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1 user-scalable=no"/>
+    <Script
+        strategy={'afterInteractive'}
+        src="https://www.google.com/recaptcha/enterprise.js?render=6LdOX5QqAAAAALaHFmLKGfZQLPI6RpuX1bU10r9b">
+
+    </Script>
+
+    <Script>
+      {`
+      
+       function onClick(e) {
+    e.preventDefault();
+    grecaptcha.enterprise.ready(async () => {
+      const token = await grecaptcha.enterprise.execute('6LdOX5QqAAAAALaHFmLKGfZQLPI6RpuX1bU10r9b', {action: 'LOGIN'});
+    });
+  }
+      
+      
+      `}
+
+    </Script>
+
+
 
   </Head>
 
@@ -19,6 +42,6 @@ export default function App({ Component, pageProps }: AppProps) {
 
   <Component {...pageProps} />
 
-  </>
+</>
   );
 }
