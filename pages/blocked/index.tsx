4e091af7c0fd4941f5e41cd6210390f5a4d46d08
Rocolusso <prolight@prolights-MacBook-Pro.local>
2024-12-17 20:50:50 +0200

    added blocker all countries without moldova// updated 4

--- pages/blocked/index.tsx
+++ pages/blocked/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
     return (
-        <div className={'flex justify-center items-center'}>
-            <h1>Access Denied</h1>
+        <div className={'flex flex-col gap-10 h-screen  justify-center items-center'}>
+            <h1 className={'text-xl uppercase font-bold'}>Access Denied</h1>
             <p>We’re sorry, but access to this website is not allowed from your location.</p>
         </div>
     );
