a0adbdd834b4f46fce1cbf3e8cae091f15b7ae1b
Rocolusso <prolight@prolights-MacBook-Pro.local>
2025-03-18 18:43:57 +0200

    added vercel SpeedInsights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 import GoogleTagManager from "@/components/GoogleTagManager";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return(
@@ -21,6 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   <Component {...pageProps} />
   <Analytics/>
+  <SpeedInsights />
 
 </>
   );
