3c25c8de88f71f8ba06714ee94ab3088210f23ab
Rocolusso <prolight@prolights-MacBook-Pro.local>
2025-05-10 01:37:09 +0300

    backup package.json 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -739,7 +739,7 @@ export default function Home({ posts }: { posts: { title: string, url: string }[
 export async function getStaticProps() {
   return {
     props: {
-      posts: [0]
+      posts: [0,1]
     },
   };
 }
