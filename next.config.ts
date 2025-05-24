e1f1cfb4a9a5e4d9000092843ed6e7412686875d
Rocolusso <rocolusso@icloud.com>
2025-05-24 03:42:16 +0300

    updated config to fix issue with client error with search console

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  generateBuildId: async () => `${Date.now()}` // Forces new build ID on each deploy
+  ,
+
 };
 
 export default nextConfig;
