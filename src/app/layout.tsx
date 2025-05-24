9eabc58398e88e0ecdf7859e428bb296a1100fe1
Rocolusso <rocolusso@icloud.com>
2025-05-24 04:26:53 +0300

    added index canonical url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta httpEquiv="content-language" content="ru" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="canonical" href="https://meddera.md/" />
       </head>
       <GoogleAnalytics />
       <Analytics />
