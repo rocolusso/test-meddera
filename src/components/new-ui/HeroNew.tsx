e6d7f8db4cf4cb3927aedf6fa8b8b60a92985620
Rocolusso <rocolusso@icloud.com>
2025-11-17 15:25:26 +0200

    updated hero banner speed optimisation

--- src/components/new-ui/HeroNew.tsx
+++ src/components/new-ui/HeroNew.tsx
@@ -6,13 +6,24 @@ function HeroNew() {
   return (
     <section className="relative mx-auto  h-[250px] sm:h-[40vh]  overflow-hidden shadow-lg  border-b border-gray-400">
       <div className="absolute inset-0 top-0 left-0 right-0">
+        {/* <Image */}
+        {/* eslint-disable-next-line max-len */}
+        {/*  className="object-contain no-repeat mx-auto object-center w-full h-full sm:shadow-2xl" */}
+        {/*  src={baner1} */}
+        {/*  alt="pintea" */}
+        {/*  width={2200} */}
+        {/*  height={2000} */}
+        {/*  priority */}
+        {/* /> */}
         <Image
-          className="object-contain no-repeat mx-auto object-center w-full h-full sm:shadow-2xl"
           src={baner1}
           alt="pintea"
           width={2200}
           height={2000}
           priority
+          fetchPriority="high"
+          // className="object-cover w-full h-full sm:shadow-2xl"
+          className="object-contain no-repeat mx-auto object-center w-full h-full sm:shadow-2xl"
         />
         {/* <div className="absolute inset-0 bg-black opacity-20" /> */}
       </div>
