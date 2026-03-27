ceb5ff6fac0c9ec30ebc08fc05a72ddcddd2c2ca
Rocolusso <rocolusso@icloud.com>
2026-03-27 16:06:16 +0200

    added Microsoft Clarity tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,15 @@ export default function RootLayout({
       addressCountry: 'MD',
     },
   };
+
+
+  const microsoftClarity =`
+  (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "w2ch4v2j05");
+  `
   return (
     <html lang="ru">
       <head>
@@ -58,6 +67,10 @@ export default function RootLayout({
           /* eslint-disable-next-line react/no-danger */
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
+          <script type="text/javascript"
+              /* eslint-disable-next-line react/no-danger */
+                  dangerouslySetInnerHTML={{ __html: JSON.stringify(microsoftClarity) }}
+          />
       </head>
       {/* <GoogleAnalytics /> */}
       <Analytics />
