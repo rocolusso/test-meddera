27a39e347db1ce2a971d0892ce492e8396f835ab
Rocolusso <rocolusso@icloud.com>
2026-03-27 19:46:23 +0200

    updated CSP headers/ upd5

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const getCsp = (isDev: boolean) => {
     "frame-ancestors 'none'",
     `script-src 'self' 'unsafe-inline'${isDev ? " 'unsafe-eval'" : ''} https://www.googletagmanager.com https://www.google-analytics.com https://connect.facebook.net https://clarity.ms https://www.clarity.ms https://scripts.clarity.ms https://analytics.ahrefs.com https://vercel.live https://googleads.g.doubleclick.net`,
     "style-src 'self' 'unsafe-inline'",
-    "img-src 'self' data: blob: https://www.google-analytics.com https://www.googletagmanager.com https://connect.facebook.net https://www.facebook.com https://www.google.com https://www.google.pl https://www.google.md https://googleads.g.doubleclick.net https://c.clarity.ms https://purecatamphetamine.github.io",
+    "img-src 'self' data: blob: https://www.google-analytics.com https://www.googletagmanager.com https://connect.facebook.net https://www.facebook.com https://www.google.com https://www.google.pl https://www.google.md https://googleads.g.doubleclick.net https://c.clarity.ms https://c.bing.com https://purecatamphetamine.github.io",
     "font-src 'self' data: https:",
     "connect-src 'self' https://www.google-analytics.com https://analytics.google.com https://region1.analytics.google.com https://www.googletagmanager.com https://www.google.com https://www.facebook.com https://graph.facebook.com https://connect.facebook.net https://clarity.ms https://www.clarity.ms https://scripts.clarity.ms https://a.clarity.ms https://k.clarity.ms https://stats.g.doubleclick.net https://analytics.ahrefs.com",
     "frame-src 'self' https://www.googletagmanager.com https://www.facebook.com https://maps.google.com https://www.google.com https://vercel.live",
